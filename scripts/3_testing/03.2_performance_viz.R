3e933c8a74bd6a971afa6b81cc84ade141968a35
nlbegotka <nlbegotka@gmail.com>
2024-05-14 11:12:26 -0400

    testing plot code

--- scripts/3_testing/03.2_performance_viz.R
+++ scripts/3_testing/03.2_performance_viz.R
@@ -14,3 +14,25 @@ load("output/3_aucs/auc_assessment.rda")
 
 # Functions
 source("scripts/0_functions/00_create_directory.R")
+
+#-------------------------------------------------------------------------------
+# 1. Plot
+#-------------------------------------------------------------------------------
+
+# Create df 
+model_names <- names(aucs)
+auc_values <- c(0.8097, 0.806, 0.7998, 0.5107, 0.5627, 0.8081, 0.8102, 0.81)
+auc_df <- data.frame(model = model_names, auc = auc_values) %>%  
+  arrange(desc(auc))
+auc_df$model <- fct_reorder(auc_df$model, auc_df$auc, .desc = TRUE)
+
+# Create a barplot 
+ggplot(auc_df, aes(x = model, y = auc)) +
+  geom_bar(stat = "identity", fill = "#B03A2E") +
+  geom_text(aes(label = round(auc, 4)), vjust = -0.5) +  # Add labels above bars
+  labs(title = "AUC Values for Different Models",
+       x = "Model", y = "AUC") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for better readability
+
+# Save
+
